6980b57435da25095afd5f65dafb34ee7ac2177c
HaileA2 <hailea536@gmail.com>
2025-07-27 22:17:58 +0200

    feat:swagger setup are completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('My Portfolio API')
+    .setDescription('API documentation for Haile’s portfolio')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
